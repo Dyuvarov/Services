53efdb164404c782666723061a9ee7f1f929f596
Ullhor Greyiron <ugreyiro@er-d1.kzn.21-school.ru>
2021-03-06 12:33:31 +0300

    Day7_1

--- setup.sh
+++ setup.sh
@@ -1,42 +1,24 @@
 #!/bin/bash
 
-echo "minikube delete"
 minikube delete
-echo "minikube start"
-minikube start --vm-driver=virtualbox
-echo "eval $(minikube docker-env)"
+minikube start --vm-driver=virtualbox --memory=4096 CPUs=3
 eval $(minikube docker-env)
-echo "minikube addons enable metallb"
+minikube addons enable dashboard
 minikube addons enable metallb
-echo "kubectl apply -f srcs/configmap.yaml"
 kubectl apply -f srcs/configmap.yaml
 ##############IMAGES##################
-echo "docker build -t mysql_image srcs/mysql"
 docker build -t mysql_image srcs/mysql
-echo "docker build -t influxdb_image srcs/influxdb"
 docker build -t influxdb_image srcs/influxdb
-echo "docker build -t nginx_image srcs/nginx"
 docker build -t nginx_image srcs/nginx
-echo "docker build -t wp_image srcs/wordpress"
 docker build -t wp_image srcs/wordpress
-echo "docker build -t phpmyadmin_image srcs/phpmyadmin"
 docker build -t phpmyadmin_image srcs/phpmyadmin
-echo "docker build -t ftps_image srcs/ftps"
 docker build -t ftps_image srcs/ftps
-echo "docker build -t grafana_image srcs/grafana"
 docker build -t grafana_image srcs/grafana
 ##############SERVICES#################
-echo "kubectl apply -f srcs/mysql/mysql.yaml"
 kubectl apply -f srcs/mysql/mysql.yaml
-echo "kubectl apply -f srcs/influxdb/influxdb.yaml"
 kubectl apply -f srcs/influxdb/influxdb.yaml
-echo "kubectl apply -f srcs/nginx/nginx.yaml"
 kubectl apply -f srcs/nginx/nginx.yaml
-echo "kubectl apply -f srcs/wordpress/wp.yaml"
 kubectl apply -f srcs/wordpress/wp.yaml
-echo "kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml"
 kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
-echo "kubectl apply -f srcs/ftps/ftps.yaml"
 kubectl apply -f srcs/ftps/ftps.yaml
-echo "kubectl apply -f srcs/grafana/grafana.yaml"
 kubectl apply -f srcs/grafana/grafana.yaml
--- srcs/phpmyadmin/start.sh
+++ srcs/phpmyadmin/start.sh
@@ -1,3 +1,3 @@
 #!bin/sh
-php-fpm7
-nginx -g "daemon off;"
+
+/usr/bin/supervisord -c /etc/supervisord.conf
